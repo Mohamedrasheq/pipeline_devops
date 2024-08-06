c710b771cc1d9f3fdcf4c9c82359100f3f9ca0b3
MOHAMED RASHEQ A <137920724+Mohamedrasheq@users.noreply.github.com>
2024-08-06 07:08:38 +0530

    Update LinearSearch.java

--- LinearSearch.java
+++ LinearSearch.java
@@ -24,7 +24,6 @@ class LinearSearch {
 		
         if (index == -1) 
             System.out.println("Element is not present in the array"); 
-	    System.out.println("Element is not present in the array"); 
         else
             System.out.println("Element found at position " + index); 
     } 
